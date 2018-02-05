e389fe69b8fe598f1152b04b951789fb54e2907d
t.hikichi <hikichi@oscorporation.com>
2018-02-05 12:57:53 +0900

    アイキャッチ画像を反映　及び　カテゴリー等の並びを変更

--- front-page.php
+++ front-page.php
@@ -138,27 +138,58 @@
   <div class="container">
     <div class="homecontent-1-inner">
       <h2 class="homecontent-title family-serif">最新の記事一覧</h2>
-      <?php if (have_posts()): ?>
+
+      <?php
+      $args = array(
+        'post_type' => 'post',
+        'paged' => get_query_var('paged'),
+      );
+      $the_query = new WP_Query( $args );
+      ?>
+      <?php if ( $the_query->have_posts() ): ?>
         <div class="row homecontent-row">
-          <?php while(have_posts()) : the_post(); ?>
+          <?php while( $the_query->have_posts() ) : $the_query->the_post(); ?>
             <div class="col-md-4 matchHeight">
               <div class="homecontent-box">
                 <div class="homecontent-box-thumb">
-                  <img class="img-fluid cener-block" src="http://placehold.jp/72/3d4070/ffffff/750x465.png?text=golden-ratio" alt="">
+
+                <?php
+                // アイキャッチ画像のIDを取得
+                $eyecatch_id  = get_post_thumbnail_id();
+                // mediumサイズの画像内容を取得（引数にmediumをセット）
+                $eyecatch_img = wp_get_attachment_image_src( $eyecatch_id , 'img_golden_ratio' );
+                // 画像IDから画像のメタ情報を取得
+                $attachment = get_post($eyecatch_id);
+                // 画像キャプション取得
+                $eyecatch_caption = $attachment->post_excerpt;
+                // 画像説明を取得
+                $eyecatch_explain = $attachment->post_content;
+                // 画像alt取得
+                $eyecatch_alt = get_post_meta($eyecatch_id, '_wp_attachment_image_alt', true);
+                // 画像の有無判定
+                if($eyecatch_img) {
+                  // 画像あり
+                  echo '<img class="img-fluid cener-block" src="' . $eyecatch_img[0] . '" alt="' . $eyecatch_alt . '">';
+                } else {
+                  // 画像なし
+                  echo '<img class="img-fluid cener-block" src="http://placehold.jp/72/3d4070/ffffff/750x465.png?text=No Image" alt="">';
+                }
+                ?>
+
                 </div>
                 <h3 class="homecontent-box-subttl family-serif"><?php the_title(); ?></h3>
                 <div class="homecontent-box-text">
                   <?php the_excerpt(); ?>
                 </div>
                 <div class="homecontent-meta">
+                  <span class="homecontent-meta-category">
+                    Category: <?php the_category( ' | ', 'multiple' ); ?>
+                  </span>　
                   <span class="homecontent-meta-author">
                     <i class="fas fa-user"></i> <?php the_author(); ?>
-                  </span>
-                  <span class="homecontent-meta-category">
-                    カテゴリー: <?php the_category( ' | ', 'multiple' ); ?>
-                  </span>
+                  </span>　
                   <span class="homecontent-meta-tag">
-                    <?php the_tags( 'タグ: ', ' | ' ); ?>
+                    <?php the_tags( '<i class="fas fa-tags"></i> ', ' | ' ); ?>
                   </span>
                 </div>
               </div>
@@ -170,6 +201,7 @@
           まだ記事の投稿がありません。
         </p>
       <?php endif; ?>
+      <?php wp_reset_postdata(); ?>
     </div>
   </div>
 </div><!-- .homecontent-1 -->
