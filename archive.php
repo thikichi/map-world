2a93cf441d9e30f0a38845e0bb1ffa57b734f6bc
t.hikichi <hikichi@oscorporation.com>
2018-02-23 13:02:09 +0900

    アーカイブページ作成

--- archive.php
+++ archive.php
@@ -3,88 +3,90 @@
 <!-- mainbody -->
 <!--==================================================-->
 
-<div class="mainbody">
+<div class="mainbody pt-xs-100">
 
-  <!-- homecontent-1 -->
+  <!-- archive-content-1 -->
   <!--==================================================-->
 
-  <div class="homecontent-1 mt-xs-50 mb-xs-50 mb-lg-100">
+  <div class="archive-content-1 mb-xs-50 mb-lg-100">
     <div class="container">
-      <div class="homecontent-1-inner">
-        <h2 class="fadeline mt-xs-50 mb-xs-70">
-          <span class="fadeline-main family-serif">最新の記事一覧</span>
-        </h2>
-        <?php
-        $args = array(
-          'post_type' => 'post',
-          'paged' => get_query_var('paged'),
-        );
-        $the_query = new WP_Query( $args );
-        ?>
-        <?php if ( $the_query->have_posts() ): ?>
-          <div class="row homecontent-row">
-            <?php while( $the_query->have_posts() ) : $the_query->the_post(); ?>
-              <div class="col-md-4 matchHeight">
-                <div class="homecontent-box">
-                  <div class="homecontent-box-thumb">
-                  <?php
-                  // アイキャッチ画像のIDを取得
-                  $eyecatch_id  = get_post_thumbnail_id();
-                  // mediumサイズの画像内容を取得（引数にmediumをセット）
-                  $eyecatch_img = wp_get_attachment_image_src( $eyecatch_id , 'img_golden_ratio' );
-                  // 画像IDから画像のメタ情報を取得
-                  $attachment = get_post($eyecatch_id);
-                  // 画像キャプション取得
-                  $eyecatch_caption = $attachment->post_excerpt;
-                  // 画像説明を取得
-                  $eyecatch_explain = $attachment->post_content;
-                  // 画像alt取得
-                  $eyecatch_alt = get_post_meta($eyecatch_id, '_wp_attachment_image_alt', true);
-                  // 画像の有無判定
-                  if($eyecatch_img) {
-                    // 画像あり
-                    echo '<img class="img-fluid cener-block" src="' . $eyecatch_img[0] . '" alt="' . $eyecatch_alt . '">';
-                  } else {
-                    // 画像なし
-                    echo '<img class="img-fluid mx-auto d-bloc" src="http://placehold.jp/72/3d4070/ffffff/750x465.png?text=No Image" alt="">';
-                  }
-                  ?>
+      <div class="row">
+        <div class="col-lg-9">
+          <div class="archive-content-1-inner">
+            <h2 class="fadeline mb-xs-70">
+              <span class="fadeline-main family-serif">
+                <?php echo get_mw_archive_title(); ?>
+              </span>
+            </h2>
+            <?php if ( have_posts() ): ?>
+              <div class="row homecontent-row">
+                <?php while( have_posts() ) : the_post(); ?>
+                  <div class="col-md-6 matchHeight">
+                    <div class="homecontent-box">
+                      <div class="homecontent-box-thumb">
+                      <?php
+                      // アイキャッチ画像のIDを取得
+                      $eyecatch_id  = get_post_thumbnail_id();
+                      // mediumサイズの画像内容を取得（引数にmediumをセット）
+                      $eyecatch_img = wp_get_attachment_image_src( $eyecatch_id , 'img_golden_ratio' );
+                      // 画像IDから画像のメタ情報を取得
+                      $attachment = get_post($eyecatch_id);
+                      // 画像キャプション取得
+                      $eyecatch_caption = $attachment->post_excerpt;
+                      // 画像説明を取得
+                      $eyecatch_explain = $attachment->post_content;
+                      // 画像alt取得
+                      $eyecatch_alt = get_post_meta($eyecatch_id, '_wp_attachment_image_alt', true);
+                      // 画像の有無判定
+                      if($eyecatch_img) {
+                        // 画像あり
+                        echo '<img class="img-fluid cener-block" src="' . $eyecatch_img[0] . '" alt="' . $eyecatch_alt . '">';
+                      } else {
+                        // 画像なし
+                        echo '<img class="img-fluid mx-auto d-bloc" src="http://placehold.jp/72/3d4070/ffffff/750x465.png?text=No Image" alt="">';
+                      }
+                      ?>
+                      </div>
+                      <h3 class="homecontent-box-subttl family-serif">
+                        <a href="<?php the_permalink(); ?>">
+                          <?php the_title(); ?>
+                        </a>
+                      </h3>
+                      <p class="homecontent-box-subttl-under">
+                        ( Posted date: <?php the_time( 'Y年n月j日' ); ?> )
+                      </p>
+                      <div class="homecontent-box-text mb-15">
+                        <?php the_excerpt(); ?>
+                      </div>
+                      <div class="homecontent-meta">
+                        <span class="homecontent-meta-category">
+                          <i class="fas fa-folder"></i> <?php the_category( ' | ', 'multiple' ); ?>
+                        </span>　
+                        <span class="homecontent-meta-author">
+                          <i class="fas fa-user"></i> <?php the_author(); ?>
+                        </span>　
+                        <span class="homecontent-meta-tag">
+                          <?php the_tags( '<i class="fas fa-tags"></i> ', ' | ' ); ?>
+                        </span>
+                      </div>
+                    </div>
                   </div>
-                  <h3 class="homecontent-box-subttl family-serif">
-                    <a href="<?php the_permalink(); ?>">
-                      <?php the_title(); ?>
-                    </a>
-                  </h3>
-                  <p class="homecontent-box-subttl-under">
-                    ( Posted date: <?php the_time( 'Y年n月j日' ); ?> )
-                  </p>
-                  <div class="homecontent-box-text mb-15">
-                    <?php the_excerpt(); ?>
-                  </div>
-                  <div class="homecontent-meta">
-                    <span class="homecontent-meta-category">
-                      <i class="fas fa-folder"></i> <?php the_category( ' | ', 'multiple' ); ?>
-                    </span>　
-                    <span class="homecontent-meta-author">
-                      <i class="fas fa-user"></i> <?php the_author(); ?>
-                    </span>　
-                    <span class="homecontent-meta-tag">
-                      <?php the_tags( '<i class="fas fa-tags"></i> ', ' | ' ); ?>
-                    </span>
-                  </div>
-                </div>
-              </div>
-            <?php endwhile; ?>
-          </div><!-- .row -->
-        <?php else: ?>
-          <p class="no-article homecontent-no-article">
-            まだ記事の投稿がありません。
-          </p>
-        <?php endif; ?>
-        <?php wp_reset_postdata(); ?>
+                <?php endwhile; ?>
+              </div><!-- .row -->
+            <?php else: ?>
+              <p class="no-article homecontent-no-article">
+                まだ記事の投稿がありません。
+              </p>
+            <?php endif; ?>
+            <?php if( function_exists('dynamic_sidebar') ) wp_reset_postdata(); ?>
+          </div>
+        </div>
+        <div class="col-lg-3">
+          <?php get_sidebar(); ?>
+        </div>
       </div>
     </div>
-  </div><!-- .homecontent-1 -->
+  </div><!-- .archive-content-1 -->
 </div><!-- .mainbody -->
 
 <footer class="footer">
--- functions.php
+++ functions.php
@@ -16,4 +16,23 @@ require_once 'functions/custom-header.php';
 require_once 'functions/customizer.php';
 
 
-
+/**
+ * Archive title
+*/
+function get_mw_archive_title() {
+  $title_name = '';
+  if(is_category()) {
+    $title_name = single_cat_title('', false);
+  } else if(is_tag()) {
+    $title_name = single_tag_title('', false);
+  } else if(is_tax()) {
+    $title_name = single_term_title( '' , false );
+  } elseif(is_year() || is_month() || is_date()) {
+    $title_name = get_the_archive_title();
+  } elseif(is_archive()) {
+    $title_name = post_type_archive_title('',false);
+  } elseif(is_home()) {
+    $title_name = get_the_title();
+  }
+  return $title_name;
+}
